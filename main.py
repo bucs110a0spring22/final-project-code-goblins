ca19cd39c95a529dee404070d7f8e5ad7fb6c33b
WhileCrocodile <98533784+WhileCrocodile@users.noreply.github.com>
2022-03-22 13:31:24 +0000

    mylist, team member names in readme

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ def main():
 # https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
     main()
+
+mylist = []
+for i in range(4):
+  listnum = int(input("Please enter a number: "))
+  mylist.append(listnum)
+for i in range(4):
+  print(mylist[i])
+mylist[0], mylist[3] = mylist[3], mylist[0]
+print("")
+for i in range(4):
+  print(mylist[i])
